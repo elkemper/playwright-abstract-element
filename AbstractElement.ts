537a32eb2550ebab7244703d02b532f33dc96913
Vladislav Belous <elkemper@users.noreply.github.com>
2024-09-02 23:41:43 +0300

    add get method

--- AbstractElement.ts
+++ AbstractElement.ts
@@ -1,5 +1,9 @@
+import { Locator } from "playwright-core";
+
 export default abstract class AbstractElement {
   protected abstract selector: string
   constructor(protected parent?: AbstractElement){
   }
+
+  protected abstract get element(): Locator
 }
